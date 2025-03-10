b80afb17e93aa62fd515a34a0032d3ec4295faf9
alangt22 <alansilva2896@gmail.com>
2025-03-10 11:59:24 -0300

    Kalyn ajustes 21

--- src/componentes/nav/Nav.jsx
+++ src/componentes/nav/Nav.jsx
@@ -26,11 +26,11 @@ function Nav() {
   return (
     <nav className={`menu-nav ${isSticky ? 'sticky' : ''}`}>
       <ul className='menu-list-nav'>
-        <li><a href="/produtos"><FaBox size={20} style={{ marginRight: '8px' }} /> Todos os produtos</a></li>
+{/*         <li><a href="/produtos"><FaBox size={20} style={{ marginRight: '8px' }} /> Todos os produtos</a></li>
         <li><a href="/acessorios"><FaTag size={20} style={{ marginRight: '8px' }} /> Acessórios</a></li>
-        <li><a href="/cabelo"><FaRegSmileBeam size={20} style={{ marginRight: '8px' }} /> Cabelo</a></li>
+        <li><a href="/cabelo"><FaRegSmileBeam size={20} style={{ marginRight: '8px' }} /> Cabelo</a></li> */}
         <li><a href="/maquiagem"><FaBrush size={20} style={{ marginRight: '8px' }} /> Maquiagem</a></li>
-        <li><a href="/paleta"><FaPalette size={20} style={{ marginRight: '8px' }} /> Paleta</a></li>
+{/*         <li><a href="/paleta"><FaPalette size={20} style={{ marginRight: '8px' }} /> Paleta</a></li> */}
         <li><a href="/olhos"><FaEye size={20} style={{ marginRight: '8px' }} /> Olhos</a></li>
       </ul>
     </nav>
